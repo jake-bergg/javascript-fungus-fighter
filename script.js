aad47d5f7dbe71513a0ed4898b8d72e4d8de9219
Jake Berg <jberg543@outlook.com>
2024-11-04 22:39:57 -0600

    added some global variables as well as the beginnings of a changeState function and a buttonClick function

--- script.js
+++ script.js
@@ -3,18 +3,27 @@
 // Example:
 // let fungusHP = 100;
 
-function onReady() {
-    console.log("Ready to go!")
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+
+// ! set fungus HP and player AP to 100
+let fungusHP = 100
+let playerAP = 100
+// ! set attacks stats to const objects in an array
+const attacks = [
+    { apCost: 20, damage: 10 },
+    { apCost: 50, damage: 30 },
+    { apCost: 100, damage: 70 }
+  ]
+
+
+// ! STATE 
+function changeState(){
+    // ! update player AP and fungus HP bars
+    // console.log(document.getElementById("ap-text").value) 
+
 }
 
-
-onReady()
+// ! function to affect dom with button clicks
+function buttonClick(event){
+    console.log('Player AP is at: ', playerAP)
+    console.log('Fungus HP is at: ', fungusHP)
+}
